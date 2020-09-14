361a2ab4c24aecfcbbcdaaaa58bf8a7098ab9194
Denis Yatsenko <yatsenko.denis46@gmail.com>
2020-09-14 23:07:15 +0300

    colliderEdge prefab instantiate update
    add prefab to resources folder

--- SS_Platformer_URP/Assets/SS_Tutorial/Characters/CharacterControl/CharacterControl.cs
+++ SS_Platformer_URP/Assets/SS_Tutorial/Characters/CharacterControl/CharacterControl.cs
@@ -41,8 +41,7 @@ namespace ss_tutorial
         public AnimationProgress animationProgress;
         public AIProgress aiProgress;
         public DamageDetector damageDetector;
-
-        public GameObject ColliderEdgePrefab;
+       // public GameObject ColliderEdgePrefab;
         public List<GameObject> BottomSpheres = new List<GameObject>();
         public List<GameObject> FrontSpheres = new List<GameObject>();
 
@@ -228,7 +227,7 @@ namespace ss_tutorial
 
         public GameObject CreateEdgeSphere(Vector3 pos)
         {
-            GameObject obj = Instantiate(ColliderEdgePrefab, pos, Quaternion.identity);
+            GameObject obj = Instantiate(Resources.Load("ColliderEdge", typeof(GameObject)), pos, Quaternion.identity) as GameObject; 
             return obj;
         }
 
